6336183115a4d0ad288216a9eddbad06878d52c1
saidejp <said.ejp@gmail.com>
2019-03-08 16:03:13 -0600

    Change precision in mixture model

--- mixture_promises.R
+++ mixture_promises.R
@@ -67,13 +67,13 @@ m_1 <- jags.model(textConnection(mix_1),
 
 
 # Burning m_1 -------------------------------------------------------------
-update(m_1, 2e3)
+update(m_1, 5e3)
 
 
 # Sampling from posterior m_1 ---------------------------------------------
 post_m_1 <- coda.samples(m_1,
                          variable.names = params,
-                         n.iter = 10e3)
+                         n.iter = 30e3)
 
 
 # Summary m_1 -------------------------------------------------------------
@@ -117,7 +117,7 @@ beta[2] <- (1 - mu2) * lambda2
 
 # Priors for mu1 and mu2
 mu1 ~ dunif(0, 1)
-lambda1 ~ dunif(40, 800)
+lambda1 ~ dunif(4, 100)
 
 mu2 ~ dunif(mu1, 1)
 lambda2 ~ dunif(4, 100)
@@ -129,7 +129,6 @@ lambda2 ~ dunif(4, 100)
 # Parameters m_2 ----------------------------------------------------------
 params2 <- c("z", "mu1", "lambda1", "mu2", "lambda2", "phi")
 
-
 # Compile m_2 -------------------------------------------------------------
 set.seed(1298)
 m_2 <- jags.model(textConnection(mix_2),
@@ -137,13 +136,13 @@ m_2 <- jags.model(textConnection(mix_2),
                   n.chains = 3)
 
 # Burning m_2 -------------------------------------------------------------
-update(m_2, 2e3)
+update(m_2, 5e3)
 
 
 # Sampling from posterior m_2 ---------------------------------------------
 post_m_2 <- coda.samples(m_2,
                          variable.names = params2,
-                         n.iter = 10e3)
+                         n.iter = 20e3)
 
 
 # Summary m_2 -------------------------------------------------------------
@@ -155,86 +154,9 @@ dic_m2 <- dic.samples(m_2, n.iter = 5e3)
 
 
 # Comparing m_1 - m_2 -----------------------------------------------------
+dic_m1
+dic_m2
 dic_m1 - dic_m2
-
-
-# Model 2 with differences on logit scale ---------------------------------
-
-mix_2_logit <- "model {
-
-# Each person belongs to one of two latent groups
-for(i in 1:n_subjects) {
-  z[i] ~ dbern(phi)  # phi is the base rate
-  z1[i] <- z[i] + 1
-}
-
-# Uninformative prior on base rate
-phi ~ dbeta(5, 5)
-
-
-# likelihood
-
-for(i in 1:n_subjects) {
-  for(j in 1:n_trials) {
-    d[i, j] ~ dbern(theta[i, j, z1[i]])
-    theta[i, j, 1] ~ dbeta(alpha[1], beta[1])
-    theta[i, j, 2] ~ dbeta(alpha[2], beta[2])
-  }
-}
-
-# Transformation to group mean and precision
-alpha[1] <- mu_h * lambda_h
-beta[1] <- (1 - mu_h) * lambda_h
-
-# Additivity on logit scale
-logit(mu_dis) <- logit(mu_h) - mu_diff
-
-alpha[2] <- mu_dis * lambda_dis
-beta[2] <- (1 - mu_dis) * lambda_dis
-
-# Priors 
-mu_h ~ dbeta(1, 1)
-mu_diff ~ dnorm(0, 0.5)T(0,) #Constrained to be positive
-
-lambda_h ~ dunif(40, 800)
-lambda_dis ~ dunif(4, 100)
-
-}"
-
-
-# Parameters logit scale --------------------------------------------------
-params2_logit <- c("z", "mu_h", "mu_dis", "mu_diff", "lambda_h",
-                   "lambda_dis", "phi")
-
-
-# Compiling m_2 logit -----------------------------------------------------
-set.seed(9876)
-m_2_logit <- jags.model(textConnection(mix_2_logit),
-                        data = data_jags,
-                        n.chains = 3)
-
-
-# Burning m_2 logit -------------------------------------------------------
-update(m_2_logit, 5e3)
-
-
-
-# Sampling from posterior m_2 logit ---------------------------------------
-post_m_2_logit <- coda.samples(m_2_logit, 
-                               variable.names = params2_logit,
-                               n.iter = 20e3)
-
- # Summary m_2 logit -------------------------------------------------------
-summary(post_m_2_logit)
-
-
-# DIC m_2 logit -----------------------------------------------------------
-dic_m2_logit <- dic.samples(m_2_logit, 5e3)
-
-
-# Comparing m_2 - m_2 logit -----------------------------------------------
-dic_m2 - dic_m2_logit
-dic_m2_logit - dic_m1
 
 
 # Two mixtures m_2 --------------------------------------------------------
