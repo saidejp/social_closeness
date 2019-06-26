f51eea2bbe16ad57b00330dc06981332ecaea591
saidejp <said.ejp@gmail.com>
2019-06-25 20:59:44 -0500

    Analysis of broken promises

--- varying_effects_promises.R
+++ varying_effects_promises.R
@@ -15,6 +15,7 @@ pacman::p_load(tidyverse, brms)
 
 d_ord %>% 
   mutate(pay_back = ifelse(response == 1, "Pay back", "Not to pay back")) %>% 
+  filter(dec_partner == "Trust") %>% 
   ggplot(aes(x = prom_ord, fill = pay_back)) +
   geom_bar(position = "fill") + 
   labs(fill = NULL,
@@ -121,6 +122,82 @@ id <- cases %>%
   pull(id)
 
 clust <- hclust(dist(x), method = "ward.D", members = id)
-p_clust <- plot(clust)
+plot(clust)
+
+
+
+high <- c(32, 15, 6, 13, 38, 36, 20, 19, 7, 14, 23, 28, 37, 42, 31, 3, 9, 10, 40)
+low <- c(33, 16, 29, 12, 25, 30, 39, 27, 24, 22, 17, 11, 2, 5, 35, 26, 21, 1, 8, 
+         41, 34, 4, 18)
+
+data3 <- data3 %>% 
+  mutate(group = ifelse(id %in% high, "Unselfish", "Selfish") %>% 
+           factor(levels = c("Unselfish", "Selfish")))
+
+data3 %>% 
+  group_by(group) %>% 
+  summarise(mean = mean(response),
+            sd = sd(response),
+            sum = sum(response),
+            n = n())
+
+prop.test(c(286, 240), n = c(342, 414))
+
+
+
+
+# Analyzing broken promises -----------------------------------------------
+
+broken <- d_ord %>% 
+  filter(prom_ord == "Always", dec_partner == "Trust") %>% 
+  mutate(broken = if_else(response == 1, 0, 1) %>% 
+           as.integer(),
+         part_ord = factor(part_ord, ordered = F))
+
+  
+broken %>% 
+  group_by(part_ord) %>% 
+  summarise(mean = mean(response),
+            sd = sd(response),
+            sum = sum(response),
+            n = n(),
+            broken = n - sum)
+
+
+# Modeling Broken Promises ------------------------------------------------
+
+fit2 <- brm(broken ~ -1 + part_ord + (-1 + part_ord | id),
+            data = broken,
+            family = bernoulli("logit"),
+            prior = prior2,
+            sample_prior = "yes",
+            chains = 4,
+            cores = 4,
+            control = list(adapt_delta = 0.95))
+
+
+
+# BF for the effect of social closeness -----------------------------------
+
+hypothesis(fit2, "part_ordComputer - ((part_ordConfederate + part_ordFriend) / 2) > 0")
+hypothesis(fit2, "part_ordConfederate - part_ordFriend > 0")
+
+
+
+# Estimates in probability scale ------------------------------------------
+
+samples_fit2 <- posterior_samples(fit2)[, 1:3] %>% 
+  mutate_all(inv_logit_scaled) 
+
+names(samples_fit2) <- c("Computer", "Confederate", "Friend")
+
+
+bayesplot::mcmc_intervals(
+  samples_fit2,
+  prob_outer = 0.97
+  ) +
+  labs(x = "Probability of a Broken Promise") +
+  geom_vline(xintercept = 0, lty = 2, col = "gray") +
+  my_theme
 
 
